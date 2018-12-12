6f622dc608773d65ab1efd0c699c9c039ce7e4bb
Fan Huang <fan.huang.33@gmail.com>
2018-12-12 10:06:00 -0500

    base design

--- posts/static/js/script.js
+++ posts/static/js/script.js
@@ -5,3 +5,14 @@ document.querySelector('.navbar-burger').addEventListener('click', function() {
     this.classList.toggle('is-active');
     mobileMenu.classList.toggle('is-active');
 });
+
+
+let newPost = document.querySelector('#new-post.navbar-item');
+document.querySelector('#new-post.navbar-item').addEventListener('click', toggleModal );
+
+document.querySelector('.modal-close').addEventListener('click', toggleModal );
+document.querySelector('.modal-background').addEventListener('click', toggleModal);
+function toggleModal() {
+    let modal = document.querySelector('.modal');
+    modal.classList.toggle('is-active');
+}
